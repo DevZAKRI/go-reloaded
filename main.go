702a7904d4323a54ace3d2ab1db4130b74ea2319
mostafa zakri <mostafazakri3@gmai.com>
2024-09-05 17:08:46 +0100

    some change

--- main.go
+++ main.go
@@ -3,43 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
-
-	reloadgo "reloadgo/modFunc"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// if len(os.Args[1:]) < 2 {
-	// 	fmt.Println("Missing argument!!")
-	// 	return
-	// } else if len(os.Args[1:]) > 2 {
-	// 	fmt.Println("You have more argument than required")
-	// 	return
-	// }
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run main.go <input_file> <output_file names")
+		return
+	}
 	inputFile := os.Args[1]
-	// outputFile := os.Args[2]
-	// var ArrangedText [][]string
-	var fileContent []string
-	word := ""
+	outputFile := os.Args[2]
 	file, err := os.Open(inputFile)
 	if err != nil {
-		fmt.Println("no such a file exist")
-	} else {
-		content, err := os.ReadFile(file.Name())
-		if err != nil {
-			fmt.Println("error reading file")
-		} else {
-			for _, val := range content {
-				if val == ' ' {
-					fileContent = append(fileContent, word)
-					word = ""
-				}
-				word += string(val)
-
-			}
-			fileContent = append(fileContent, word)
-			file.Close()
-		}
-		afterHex := reloadgo.HexChange(fileContent)
-		fmt.Println(afterHex)
+		fmt.Println("Error opening file:", err)
+		return
 	}
+	defer file.Close()
+
+	content, err := os.ReadFile(file.Name())
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	fmt.Println(content)
+	fileContent := strings.Fields(string(content))
+	newFileContent := []string{}
+
+	for i := 0; i < len(fileContent); i++ {
+		val := fileContent[i]
+		if i < len(fileContent)-1 && fileContent[i+1] == "(hex)" {
+			valDec, err := strconv.ParseInt(val, 16, 64)
+			if err != nil {
+				fmt.Println("Error converting hex to decimal:", err)
+				return
+			}
+			newFileContent = append(newFileContent, strconv.Itoa(int(valDec)))
+			i++
+		} else {
+			newFileContent = append(newFileContent, val)
+		}
+	}
+
+	err = os.WriteFile(outputFile, []byte(strings.Join(newFileContent, " ")), 0o644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Processing complete. Output written to", outputFile)
 }
